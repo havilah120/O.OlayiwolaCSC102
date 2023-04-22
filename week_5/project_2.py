4ab100c067bf78452db53a97a1efbeacaff9e4d8
havilah120 <wemimoy2k@gmail.com>
2023-04-22 17:10:02 -0500

    (My latest week 5 commit)

---
+++ week_5/project_2.py
@@ -0,0 +1,25 @@
+print("Welcome")
+experience = float(input('Enter your years of Experience '))
+age = float(input('Enter your Age '))
+
+if experience > 25:
+    print ("Your annual tax revenue is #5600000")
+elif age >= 55:
+     print ("Your annual tax revenue is #5600000")
+
+if experience > 20:
+    print ("Your annual tax revenue is #4480000")
+elif age >= 45:
+     print ("Your annual tax revenue is #4480000")
+
+
+if experience > 10:
+    print ("Your annual tax revenue is #1500000")
+elif experience <= 10:
+    print(" Your annual tax revenue is #550000")
+elif age >= 35:
+     print ("Your annual tax revenue is #1500000")
+elif age < 35:
+     print(" Your annual tax revenue is #550000")
+
+     
